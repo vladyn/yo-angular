42d3510ba43d09d56e4fb0f977f141f7b89736a5
vladyn <vlado@vlado1.com>
2015-01-30 23:47:14 +0200

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngExercisesApp
+ * @description
+ * # ngExercisesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngExercisesApp', [
+    'ngAria',
+    'ngMessages'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngExercisesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngExercisesApp
+ */
+angular.module('ngExercisesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.layout = {
+    	columns: 2, 
+    	topBar: true, 
+    	footer: true,
+    	sidebar: true
+    }
+  });
