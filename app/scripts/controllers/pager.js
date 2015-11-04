aa33bc4be1c8cf6d5a134e6164b1e21360379ae7
vladyn <vlado@vlado1.com>
2015-11-04 23:43:59 +0200

    more view and content

--- app/scripts/controllers/pager.js
+++ app/scripts/controllers/pager.js
@@ -11,18 +11,22 @@ angular.module('ahkApp')
   .controller('PagerCtrl', function ($scope, $location) {
     var counter,
         position,
-        pageMax = 12, 
+        isItShowing,
+        pageMax = 18, 
         vm = this;
 
     vm.page = {
-        labelPrev: vm.count,
-        labelPnext: vm.count
+        labelPrev: "Предишна",
+        labelNext: "Следваща"
     };
 
     /* It monitors for direct re-load  */
     $scope.$on('page-scanner-started', function(event, args) {
         counter = args;
     });
+    
+    // It controls when to show the prev / next arrows accross the screens
+    vm.isItShowing = $location.path().lastIndexOf('view') === 1 ? true : false;
 
     vm.movePage = function movePage (direction) {
         position = direction === 'next' ? counter++ : counter--;
--- app/scripts/services/urlextractorservice.js
+++ app/scripts/services/urlextractorservice.js
@@ -4,18 +4,18 @@ angular.module('ahkApp')
   .service('urlExtractorService', function urlExtractorService($location) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     function count (from, to) {
-    	from = angular.isDefined(from) ? from : 0;
-    	
-    	to = angular.isDefined(to) ? to : $location.path().length;
+        from = angular.isDefined(from) ? from : 0;
 
-    	var location = parseInt($location.path().slice(from, to), 10);
-    	
-    	location = angular.isNumber(location) ? location : 1;
-    	
-    	return location;
+        to = angular.isDefined(to) ? to : $location.path().length;
+
+        var location = parseInt($location.path().slice(from, to), 10);
+
+        location = angular.isNumber(location) ? location : 1;
+
+        return location;
     }
     // Return the public API.
-    return({
+    return ({
         count: count
     });
   });
